a10300b0cc9f39d92949c083ea9e37daec976154
Nicolas Vignes <46595260+nvigne@users.noreply.github.com>
2024-12-02 22:19:32 +0100

    Day 1 Part 2

--- AdventOfCode1/Program.cs
+++ AdventOfCode1/Program.cs
@@ -1,26 +1,60 @@
 ﻿using StreamReader reader = new StreamReader("input.txt");
 
-List<int> column1 = new List<int>();
-List<int> column2 = new List<int>();
-string? line;
-while ((line = reader.ReadLine()) is not null)
-{
-    var numbersAsString = line.Split("   ");
-    var number1 = int.Parse(numbersAsString[0]);
-    var number2 = int.Parse(numbersAsString[1]);
+Tuple<List<int>, List<int>> GetColumns() {
+    List<int> column1 = new List<int>();
+    List<int> column2 = new List<int>();
+    string? line;
+    while ((line = reader.ReadLine()) is not null)
+    {
+        var numbersAsString = line.Split("   ");
+        var number1 = int.Parse(numbersAsString[0]);
+        var number2 = int.Parse(numbersAsString[1]);
 
-    column1.Add(number1);
-    column2.Add(number2);
+        column1.Add(number1);
+        column2.Add(number2);
+    }
+
+    column1.Sort();
+    column2.Sort();
+
+    return new Tuple<List<int>, List<int>>(column1, column2);
 }
 
-column1.Sort();
-column2.Sort();
-
-// Calculate the distance between the two columns for each row and sum all the distances
-int sum = 0;
-for (int i = 0; i < column1.Count; i++)
+void Part1()
 {
-    sum += Math.Abs(column1[i] - column2[i]);
+    var tuple = GetColumns();
+    var column1 = tuple.Item1;
+    var column2 = tuple.Item2;
+
+    // Calculate the distance between the two columns for each row and sum all the distances
+    int sum = 0;
+    for (int i = 0; i < column1.Count; i++)
+    {
+        sum += Math.Abs(column1[i] - column2[i]);
+    }
+
+    Console.WriteLine(sum);
 }
 
-Console.WriteLine(sum);
+void Part2()
+{
+    var tuple = GetColumns();
+    var column1 = tuple.Item1;
+    var column2 = tuple.Item2;
+
+    // Calculate the sum of the value from the left column is in the right column
+    int sum = 0;
+    for (int i = 0; i < column2.Count; i++)
+    {
+        if (column1.Contains(column2[i]))
+        {
+            sum += column2[i];
+        }
+    }
+
+    Console.WriteLine(sum);
+}
+
+Part2();
+
+
