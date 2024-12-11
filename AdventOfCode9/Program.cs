09eac5545a55429331dbbc8deccc22405fa3bc74
Nicolas Vignes <46595260+nvigne@users.noreply.github.com>
2024-12-11 23:15:56 +0100

    Re-wrtie logic

--- AdventOfCode9/Program.cs
+++ AdventOfCode9/Program.cs
@@ -4,9 +4,11 @@ int[] numbers = new int[0];
 var line = reader.ReadLine();
 int indexId = 0;
 int index = 0;
+int expectedSize = 0;
 foreach (var item in line)
 {
     int number = int.Parse(item.ToString());
+    expectedSize += number;
     int previousSize = numbers.Length;
     Array.Resize(ref numbers, numbers.Length + number);
     if (index % 2 == 0)
@@ -28,68 +30,65 @@ foreach (var item in line)
     }
 }
 
+Console.WriteLine(expectedSize);
+
 //Console.WriteLine(string.Join("", numbers).Replace("-1", "."));
 
+
+int valueIndex = numbers.Length - 1;
 // Let's go through the numbers from the end to the start.
-for (int i = numbers.Length - 1; i >= 0; i--)
+for (int i = 0; i < numbers.Length; i++)
 {
     if (numbers[i] == -1)
     {
-        // If we have an empty space, let's continue.
-        continue;
+        valueIndex = GetNextIndexToSwap(numbers, valueIndex);
+        if (i >= valueIndex)
+        {
+            break;
+        }
+
+        var number = numbers[valueIndex];
+
+        numbers[i] = number;
+        numbers[valueIndex] = -1;
+
+        //Console.WriteLine(string.Join("", numbers).Replace("-1", "."));
     }
-    
-    var number = numbers[i];
-    var nextAvailableIndex = GetNextAvailableSpace(numbers);
-
-    // We don't want to go to end of the array, once we have arranged everything.
-    // If we try to move the number to a place that is greater than the next available index,
-    // it means we already re-arranged everything.
-    if (nextAvailableIndex >= i)
-    {
-        break;
-    }
-
-    // We replace the space with the number.
-    numbers[nextAvailableIndex] = number;
-
-    // Then replace the number with a space.
-    numbers[i] = -1;
 }
 
-Console.WriteLine(string.Join(",", numbers).Replace("-1", ""));
+//Console.WriteLine(string.Join(",", numbers).Replace("-1", ""));
 
 // Now calculate the checksum.
-int checksum = 0;
-bool firstSpace = false;
+long checksum = 0;
+
+Console.WriteLine(numbers.Length);
+
 for (int i = 0; i < numbers.Length; i++)
 {
-    if (numbers[i] != -1)
+    if (numbers[i] >= 0)
     {
-        checksum += numbers[i] * i;
-        if (firstSpace)
-        {
-            // Just validate that we didn't let a space.
-            Console.WriteLine("WARNING: There is a space between the numbers.");
-        }
+        checksum = checked(checksum + numbers[i] * i);
     }
     else
     {
-        firstSpace = true;
+        break;
     }
 }
 
 Console.WriteLine(checksum);
 
-static int GetNextAvailableSpace(int[] numbers)
+static int GetNextIndexToSwap(int[] numbers, int max)
 {
-    for (int i = 0; i < numbers.Length; i++)
+    // Start from the end of the array.
+    // Get first number that is not -1.
+    for (int i = max; i >= 0; i--)
     {
-        if (numbers[i] == -1)
+        if (numbers[i] != -1)
         {
             return i;
         }
     }
+
     return -1;
 }
 
